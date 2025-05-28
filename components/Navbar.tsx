3b732811bd03b617f4c962e7c88b8fd0ed82ab23
yashbim <ybimsara03@gmail.com>
2025-05-28 14:22:21 +0530

    Added side bar for mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,33 +1,77 @@
+'use client'
+
 import { NAV_BAR_LINKS } from '@/constants'
-import { link } from 'fs'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
 
 const Navbar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prev) => !prev)
+  }
+
   return (
     <nav className="border-2 border-black-500 flexBetween padding-container relative z-30 py-5">
-        <Link href="/">
-            <Image src="/global_volunteer_horizontal_color.png" alt="logo" width={200} height={100} />
-        </Link>
-        <ul className="hidden h-full gap-12 lg:flex">
-            {NAV_BAR_LINKS.map((link) => (
-                <Link href={link.href} key={link.key} 
-                className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-black">
-                    {link.label}
-                </Link>
-            ))}
-        </ul>
-        
-        <Image
-         src="menu.svg"
-         alt="menu"
-         width={32}
-         height={32}
-         className="inline-block cursor-pointer lg:hidden"
-        />
+      <Link href="/">
+        <Image src="/global_volunteer_horizontal_color.png" alt="logo" width={200} height={100} />
+      </Link>
 
+      {/* Desktop Nav */}
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_BAR_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-black"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+
+      {/* Mobile Menu Button */}
+      <Image
+        src="/menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+        onClick={toggleSidebar}
+      />
+
+      {/* Sidebar for Mobile */}
+      <div
+        className={`fixed top-0 right-0 h-full w-64 bg-white shadow-lg z-40 transform transition-transform duration-300 ease-in-out ${
+          isSidebarOpen ? 'translate-x-0' : 'translate-x-full'
+        }`}
+      >
+        <div className="flex justify-end p-4">
+          <button onClick={toggleSidebar} className="text-black text-xl">✕</button>
+        </div>
+        <ul className="flex flex-col gap-6 p-6">
+          {NAV_BAR_LINKS.map((link) => (
+            <Link
+              href={link.href}
+              key={link.key}
+              onClick={toggleSidebar}
+              className="text-lg text-black hover:font-bold transition"
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      {/* Background Overlay */}
+      {isSidebarOpen && (
+        <div
+          onClick={toggleSidebar}
+          className="fixed inset-0 bg-black opacity-50 z-30 lg:hidden"
+        />
+      )}
     </nav>
   )
 }
