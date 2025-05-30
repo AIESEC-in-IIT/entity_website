8ef9a2f0f025405bf43338d2da9683da2ba2f2c0
yashbim <ybimsara03@gmail.com>
2025-05-30 11:58:39 +0530

    Added aiesec in iit text in normal lato blue with drop shadow responsive

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,11 +19,11 @@ const Hero = () => {
         priority
       />
       <div className="absolute inset-0 bg-white opacity-50"></div>
-      <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-7xl">
+      {/* <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-5xl">
         <h1
           style={{
             textShadow: '0 0.15ch 15px #333333, 0 -2px 0 #f5f5f5',
-            fontSize: '15vmin',
+            // fontSize: '15vmin',
             letterSpacing: '0.05ch', // Increased from -0.15ch
             lineHeight: 0.75,
             color: '#037ef3',
@@ -36,7 +36,7 @@ const Hero = () => {
         <h1
           style={{
             textShadow: '0 0.15ch 15px #333333, 0 -2px 0 #f5f5f5',
-            fontSize: '15vmin',
+            // fontSize: '15vmin',
             letterSpacing: '-0.1ch', // Increased from -0.15ch
             lineHeight: 0.75,
             color: '#037ef3',
@@ -45,11 +45,30 @@ const Hero = () => {
         >
           Informatics Institute of Technology
         </h1>
-      </div>
+      </div> */}
+
+<div className="absolute inset-0 flex items-center justify-center">
+  <div className="scale-wrapper text-aiesec-blue text-center hover:font-black">
+    <h1
+      className="pb-3 whitespace-nowrap text-[5vw] sm:text-[3vw] md:text-5xl"
+      style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}
+    >
+      AIESEC in
+    </h1>
+    <h1
+      className="pb-3 whitespace-nowrap text-[5vw] sm:text-[3vw] md:text-5xl"
+      style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}
+    >
+      Informatics Institute of Technology
+    </h1>
+  </div>
+</div>
+
+
       <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
         <button className="group flex items-center gap-2 px-6 py-3 bg-white bg-opacity-90 rounded-xl shadow-md hover:bg-opacity-100 transition">
           <Image src="/explore.svg" alt="Explore Icon" width={20} height={20} />
-          <span className="text-black font-medium group-hover:text-red-700 transition-colors">Explore</span>
+          <span className="text-black font-medium group-hover:text-aiesec-blue transition-colors hover:font-black">Explore</span>
         </button>
       </div>
     </div>
