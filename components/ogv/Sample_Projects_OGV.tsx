e887c28d272c4aecb89c362c03e92c486c59a7aa
yashbim <ybimsara03@gmail.com>
2025-06-03 02:10:06 +0530

    Added sample projects section in ogv page

--- components/ogv/Sample_Projects_OGV.tsx
+++ components/ogv/Sample_Projects_OGV.tsx
@@ -7,29 +7,37 @@ import { lato } from '@/constants/font_settings';
 const Sample_Projects_OGV = () => {
   return (
     <div>
-      {/* <hr className="border-aiesec-blue pb-2" /> */}
-      <div className={`${lato.className}`}>
-        <div className={`flex justify-center`}>
-          <h1 className={`font-bold text-4xl pt-5`}>Projects</h1>          
-        </div>
-        <div>
-            <section className="px-4 py-10 bg-white">
-                <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
-                    {Sample_OGV_Projects.map((program, index) => (
-                        <div className="bg-white rounded-xl shadow-lg p-6 flex flex-col text-center space-y-4 h-full">
-                            <h3 className={`text-xl font-semibold`}>
-                                {program.title}
-                            </h3>
-
-                            <div className="flex-grow pt-2">
-                                <p className=" text-gray-600 text-l">{program.description}</p>
+        <div className={`${lato.className}`}>
+            <div className={`flex justify-center`}>
+                <h1 className={`font-bold text-4xl pt-5`}>Projects</h1>          
+            </div>
+            <div>
+                <section className="px-4 py-10 bg-white">
+                    <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+                        {Sample_OGV_Projects.map((program, index) => (
+                            <div key={index} className="bg-white rounded-xl shadow-lg p-4 flex flex-col h-full">
+                                <Image
+                                    src={program.image}
+                                    alt={program.title}
+                                    width={500}
+                                    height={300}
+                                    className="object-cover rounded-md w-full h-48"
+                                />
+                                <h3 className="text-xl font-semibold mt-4 mb-2 text-center">{program.title}</h3>
+                                <p className="text-gray-600 text-sm">{program.description.replace('/n', '\n')}</p>
                             </div>
-                        </div>
-                    ))}
-                </div>
-            </section>
-        </div>
-      </div>
+                        ))}
+                    </div>
+                </section>
+            </div>
+            <div className='py-4 flex justify-center'>                    
+                <Link href='/'>
+                    <button className="px-5 py-2 bg-aiesec-blue text-white rounded-md hover:bg-blue-700 transition-transform duration-300 ease-in-out hover:scale-105">
+                        View All Projects
+                    </button>
+                </Link> 
+            </div>
+    </div>
 
     </div>
   )
--- constants/ogv_data.ts
+++ constants/ogv_data.ts
@@ -1,21 +1,21 @@
 export const Sample_OGV_Projects = [
   {
     title: 'Global Classroom',
-    image: '/product_GV-horizontal-color.png',
+    image: '/ogv/opportunities/global_classroom.png',
     description:
       'The Global Classroom project provides educational opportunities for all ages, covering literacy, math, science, and languages. Volunteers teach English in local schools, conduct skill development sessions, and support personality development and resource initiatives. They also observe teaching methods and engage with local education centers, making a meaningful impact while exploring new destinations.',
       link: '/',
   },
   {
     title: 'Skill Up',
-    image: '/product_GTe-horizontal-color.png',
+    image: '/ogv/opportunities/skill_up.png',
     description:
-      'SKILL UP project aims to impact SDG 8 by providing young people with soft and hard skills trainings, needed to qualify for a decent job, and career guidance. \n Volunteers will have the opportunity to take part in firms and company visits to get practical knowledge about the business world, interviews and discussions with business successors, awareness on new business technology and strategies, marketing and HRM knowledge regarding business through practical experience, career and personality development sessions done by professionals.',
+      'SKILL UP project aims to impact SDG 8 by providing young people with soft and hard skills trainings, needed to qualify for a decent job, and career guidance. /n Volunteers will have the opportunity to take part in firms and company visits to get practical knowledge about the business world, interviews and discussions with business successors, awareness on new business technology and strategies, marketing and HRM knowledge regarding business through practical experience, career and personality development sessions done by professionals.',
       link: '/'
   },
   {
     title: 'On the Map',
-    image: '/product_GT-horizontal-color.png',
+    image: '/ogv/opportunities/on_the_map.png',
     description:
       'The On the Map project boosts sustainable local tourism to generate revenue and improve employability. Volunteers explore cultural and historical sites, create promotional content through blogs, photos, and videos, conduct field research on the host country’s heritage, and organize workshops to enhance tourism in local communities. They also embark on long trips covering key cities, gaining valuable insights and experiences.',
       link: '/'
