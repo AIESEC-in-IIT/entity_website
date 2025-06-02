244ba09b95b6a81a85563bafefc24f141a06f692
yashbim <ybimsara03@gmail.com>
2025-06-02 22:47:08 +0530

    Modified Navbar elements

--- constants/index.ts
+++ constants/index.ts
@@ -1,13 +1,23 @@
 // NAVIGATION LIST
 
+// export const NAV_BAR_LINKS = [
+//     { href: '/', key: 'home', label: 'Home' },
+//     { href: '/', key: 'products', label: 'Our Products' },
+//     { href: '/', key: 'projects', label: 'Volunteering Projects' },
+//     { href: '/', key: 'countries', label: 'Countries' },
+//     { href: '/', key: 'about', label: 'About Us' },
+//     { href: '/', key: 'contact', label: 'Contact' },
+// ];
+
 export const NAV_BAR_LINKS = [
     { href: '/', key: 'home', label: 'Home' },
-    { href: '/', key: 'products', label: 'Our Products' },
-    { href: '/', key: 'projects', label: 'Volunteering Projects' },
-    { href: '/', key: 'countries', label: 'Countries' },
+    { href: '/', key: 'ogv', label: 'Global Volunteer' },
+    { href: '/', key: 'ogt', label: 'Global Talent & Teacher' },
+    { href: '/', key: 'events', label: 'Events' },
     { href: '/', key: 'about', label: 'About Us' },
     { href: '/', key: 'contact', label: 'Contact' },
 ];
+
 
 export const COUNTRY_FLAGS_SRC =[
     
