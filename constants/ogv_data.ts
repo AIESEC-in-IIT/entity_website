2a265aacdd2c31991189d6ab0434d0ff0ea69549
yashbim <ybimsara03@gmail.com>
2025-06-03 18:26:08 +0530

    Added remaining sdg content in projects

--- constants/ogv_data.ts
+++ constants/ogv_data.ts
@@ -26,6 +26,7 @@ export const OGV_Projects = [
   {
     title: 'Global Classroom',
     image: '/ogv/opportunities/global_classroom.png',
+    sdg_image: '/sdg_goals/sdg4.png',
     description:
       'The Global Classroom project provides educational opportunities for all ages, covering literacy, math, science, and languages. Volunteers teach English in local schools, conduct skill development sessions, and support personality development and resource initiatives. They also observe teaching methods and engage with local education centers, making a meaningful impact while exploring new destinations.',
       link: '/',
@@ -33,6 +34,7 @@ export const OGV_Projects = [
   {
     title: 'Youth 4 Impact',
     image: '/ogv/opportunities/youth_4_impact.png',
+    sdg_image: '/sdg_goals/sdg4.png',
     description:
       'YOUTH 4 IMPACT project aims to impact SDG 4 by generating awareness and fostering engagement towards the sustainable development goals and global citizen values. \n Volunteers will have the opportunity to take part in educational spaces on the Sustainable Development Goals, as well as local and global issues in collaboration with schools or local NGOs in various communities. The project will be carried out using interactive, non-formal educational methods and organizing and delivering practical spaces or events.',
       link: '/',
@@ -40,6 +42,7 @@ export const OGV_Projects = [
   {
     title: 'Fingerprint',
     image: '/ogv/opportunities/fingerprint.png',
+    sdg_image: '/sdg_goals/sdg4.png',
     description:
       'The project aims to impact SDG #4 by developing high-level cognitive and transferable skills to people of all ages through activities and/or workshops connected to art production (e.g painting, sculpture, music, theater, photography) \n Global Volunteers will be responsible for delivering immersive and artistic activities that inspire the flourishing of imagination, problem-solving, critical thinking, creativity, teamwork, and communication skills, which can be used across a range of occupational fields.',
       link: '/',
@@ -47,6 +50,7 @@ export const OGV_Projects = [
   {
     title: 'Happy Bus',
     image: '/ogv/opportunities/happy_bus.png',
+    sdg_image: '/sdg_goals/sdg4.png',
     description:
       'This project focuses on providing early childhood education to children in small towns and villages. Volunteers engage kids in learning about different cultures, languages, and social interactions through fun activities. The goal is to help children develop basic language skills and cultural awareness before they start primary school.',
       link: '/',
@@ -54,6 +58,7 @@ export const OGV_Projects = [
   {
     title: 'Explorer',
     image: '/ogv/opportunities/explorer.png',
+    sdg_image: '/sdg_goals/sdg4.png',
     description:
       "Global Volunteers will contribute to participants' development by teaching them how to work in a team, how to navigate in nature using mathematical knowledge & having an understanding of sources that surround them in nature, how to communicate in a team & how on a daily base use English.",
       link: '/',
@@ -61,6 +66,7 @@ export const OGV_Projects = [
   {
     title: 'On the Map',
     image: '/ogv/opportunities/on_the_map.png',
+    sdg_image: '/sdg_goals/sdg8.png',
     description:
       'The On the Map project boosts sustainable local tourism to generate revenue and improve employability. Volunteers explore cultural and historical sites, create promotional content through blogs, photos, and videos, conduct field research on the host country’s heritage, and organize workshops to enhance tourism in local communities. They also embark on long trips covering key cities, gaining valuable insights and experiences.',
       link: '/'
@@ -68,6 +74,7 @@ export const OGV_Projects = [
   {
     title: 'Skill Up',
     image: '/ogv/opportunities/skill_up.png',
+    sdg_image: '/sdg_goals/sdg8.png',
     description:
       'SKILL UP project aims to impact SDG 8 by providing young people with soft and hard skills trainings, needed to qualify for a decent job, and career guidance. /n Volunteers will have the opportunity to take part in firms and company visits to get practical knowledge about the business world, interviews and discussions with business successors, awareness on new business technology and strategies, marketing and HRM knowledge regarding business through practical experience, career and personality development sessions done by professionals.',
       link: '/'
@@ -75,6 +82,7 @@ export const OGV_Projects = [
   {
     title: 'Rooted',
     image: '/ogv/opportunities/rooted.png',
+    sdg_image: '/sdg_goals/sdg15.png',
     description:
       "The Rooted project empowers communities to protect natural habitats, combat biodiversity loss, and preserve wildlife. Volunteers participate in field trips, workshops, and educational sessions on ecosystems and conservation. They also support local initiatives, encouraging communities to take action for wildlife protection and biodiversity preservation. ",
       link: '/',
@@ -82,6 +90,7 @@ export const OGV_Projects = [
   {
     title: 'Green Leaders',
     image: '/ogv/opportunities/green_leaders.png',
+    sdg_image: '/sdg_goals/sdg13.png',
     description:
       "The Green Leaders project raises awareness about climate change and promotes action to reduce environmental impact. Volunteers engage in campaigns, discussions with business leaders, and hands-on activities like tree planting, plant distribution, and compost-making. This initiative empowers communities to take meaningful steps toward environmental preservation.",
       link: '/',
@@ -89,6 +98,7 @@ export const OGV_Projects = [
   {
     title: 'Aquatica',
     image: '/ogv/opportunities/aquatica.png',
+    sdg_image: '/sdg_goals/sdg14.png',
     description:
       "Global Volunteers will carry out educational spaces and fieldwork activities to prevent and stop marine and coastal ecosystems pollution. Volunteer will also be responsible for planning and executing an outreach campaigns to engage community members on the protection of marine ecosystems.",
       link: '/',
@@ -96,6 +106,7 @@ export const OGV_Projects = [
   {
     title: 'Heartbeat',
     image: '/ogv/opportunities/heartbeat.png',
+    sdg_image: '/sdg_goals/sdg3.png',
     description:
       "The project aims to impact the SDG #3 by raising awareness on prevention of non-communicable diseases and the importance of mental health. \n Global Volunteers will be involved in community outreach in the public health field by creating and running outreach activities that aim to raise awareness on prevention of non-communicable diseases, healthy lifestyle habits and mental health in the community.",
       link: '/',
@@ -103,6 +114,7 @@ export const OGV_Projects = [
   {
     title: 'Scale Up',
     image: '/ogv/opportunities/scale_up.png',
+    sdg_image: '/sdg_goals/sdg17.png',
     description:
       "The Scale Up project strengthen the capacity of local NGOs, non-profits, and foundations to drive social change. Volunteers conduct needs analysis for these organizations, identify areas for improvement in their projects, and present actionable solutions to enhance their social impact. Through collaboration and capacity-building, the project empowers these organizations to better serve and develop their communities.",
       link: '/',
@@ -110,6 +122,7 @@ export const OGV_Projects = [
   {
     title: 'Equify',
     image: '/ogv/opportunities/equify.png',
+    sdg_image: '/sdg_goals/sdg10.png',
     description:
       "The Equify project aims to address social, ethnic, gender, disability, and economic inequalities. Volunteers work to promote local initiatives that combat discrimination and raise awareness about inequality. Through community engagement and advocacy, the project strives to reduce income and opportunity gaps.",
       link: '/',
@@ -117,6 +130,7 @@ export const OGV_Projects = [
   {
     title: 'Raise Your Voice',
     image: '/ogv/opportunities/raise_your_voice.png',
+    sdg_image: '/sdg_goals/sdg5.png',
     description:
       "Global Volunteers will be responsible for creating and delivering workshops and activities aiming to engage people towards gender equality. They will also deliver activities to raise awareness on gender discrimination and how to fight it. by creating more diverse and equal spaces for all genders vie reaching and educating people.",
       link: '/',
