14e494c099d0a504b63250028ed3064e3f829c04
yashbim <ybimsara03@gmail.com>
2025-06-03 18:38:39 +0530

    Created blank ogv prjects page, integrated NavBar to page

---
+++ app/ogv/volunteering_projects/page.tsx
@@ -0,0 +1,14 @@
+import CountryBanner from "@/components/common/CountryBanner";
+import Hero from "@/components/ogv/Hero_OGV";
+import Products from "@/components/homepage/Products_Section";
+import Navbar from "@/components/ogv/Navbar_OGV";
+import Sample_Projects_OGV from "@/components/ogv/Sample_Projects_OGV";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Sample_Projects_OGV />
+    </>
+  );
+}
