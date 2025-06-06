37eb6f550dfd0aaee9b68ad89e6d416e9683ecc9
yashbim <ybimsara03@gmail.com>
2025-06-07 01:07:15 +0530

    Modernised Requirements page

--- components/ogv/Requirements.tsx
+++ components/ogv/Requirements.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { lato } from '@/constants/font_settings'
 import { raleway } from '@/constants/font_settings'
 import Image from 'next/image'
+import { GV_Requirement } from '@/constants/ogv/requirements'
+import { text } from 'stream/consumers'
 
 const Requirements = () => {
   return (
@@ -10,26 +12,19 @@ const Requirements = () => {
         <h1 className={`text-5xl font-bold`}>Requirements</h1>
       </div>
       <div>
-        <div className={`grid grid-cols-1 md:grid-cols-2 lg:gird-cols-2 gap-8 px-6 py-12 bg-white`}>
-          <div className={`grid place-items-center min-h-full`}>
-            <h1 className={`${lato.className} text-xl px-4 pb-8`}>To volunteer, the following criteria have to be satisfied.</h1>
-            <ul className={`${raleway.className} list-disc text-lg px-8 pb-8 space-y-1`}>
-              <li className={`hover:font-bold`}>Be between 18 & 30 years of age</li>
-              <li className={`hover:font-bold`}>Availability for a period of 6-8 weeks</li>
-              <li className={`hover:font-bold`}>Basic spoken English</li>
-              <li className={`hover:font-bold`}>Interest in acting on global issues</li>
-              <li className={`hover:font-bold`}>Desire to develop yourself</li>
-              <li className={`hover:font-bold`}>Open-minded by new cultures & experiences</li>
-            </ul>
-            <p className={`${lato.className} px-4`}>Additionally, to these basic requirements, the EP should be an outgoing, friendly person that can work with a group of people amidst the challenges he/she is going to face throughout the experience. This would mean not cracking under pressure and being open to feedback and criticisms which opens up the pathway to being a great leader like AIESEC promises.</p>
-          </div>
-          <div className={`flex justify-center `}>
-            <Image  className='rounded-xl shadow-md'
-            src="/mirror_selfie.jpg"
-            alt="Let's Volunteer"
-            width={500}
-            height={500}/>
-          </div>
+        <div className={`grid grid-cols-1 md:grid-cols-3 lg:gird-cols-6 gap-8 px-6 py-12 bg-white`}>
+          {GV_Requirement.map((requirement, index) => (
+            <div key={index} className={`${lato.className} items-center bg-white shadow-lg  rounded-xl p-4 flex flex-col h-full border-aiesec-blue border-2 hover:shadow-lg transition-shadow duration-300 hover:shadow-ogv-orange`}>
+              <Image 
+              src={requirement.Image}
+              alt={requirement.Text}
+              width={160}
+              height={160}
+              className={`object-cover rounded-md`}
+              />
+              <p>{requirement.Text}</p>
+            </div>
+          ))}
         </div>
       </div>
       <hr className="border-grey" />
---
+++ constants/ogv/requirements.ts
@@ -0,0 +1,21 @@
+export const GV_Requirement= [
+    {
+        Text : "Be between 18 & 30 years of age",
+        Image : "/ogv/requirements/age.png"
+    },{
+        Text : "Availability for a period of 6-8 weeks",
+        Image : "/ogv/requirements/availability.png"
+    },{
+        Text : "Basic spoken English",
+        Image : "/ogv/requirements/good_english.png"
+    },{
+        Text : "Interest in acting on global issues",
+        Image : "/ogv/requirements/global_issues.png"
+    },{
+        Text : "Desire to develop yourself",
+        Image : "/ogv/requirements/self_growth.png"
+    },{
+        Text : "Open-minded by new cultures & experiences",
+        Image : "/ogv/requirements/open_minded.png"
+    }
+]
